2ff30d8b3fc8f6bcefefb5a7d0e39af5db9459b5
Aleksandr <accgh4gb@gmail.com>
2022-09-18 22:22:27 +0300

    The second task of the fourth lesson

--- Lesson_04/Lesson_04.c
+++ Lesson_04/Lesson_04.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+int exponentiation(int num, int exp)
+{
+    int result = 0;
+    if((num == 0) || (num == 0 && exp == 0))
+    {
+        return 0;
+    }
+    else if(exp == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        result = num * exponentiation(num, exp - 1);
+    }
+    return result;
+            
+}
+
 int main()
 {
+    int num = 5;
+    int exp = 11;
+    printf("The value of the number %d to the power of %d is %d\n", num, exp, exponentiation(num,exp));
     return 0;
 }
