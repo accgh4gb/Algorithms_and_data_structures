a2555543ae48968ca9f0a64b2017b9c6df5075b0
Aleksandr <accgh4gb@gmail.com>
2022-09-19 22:10:29 +0300

    The fourth task of the fourth lesson

--- Lesson_04/Lesson_04.c
+++ Lesson_04/Lesson_04.c
@@ -54,6 +54,30 @@ int exponentiation2(int num, int exp)
     return result;
 }
 
+int KingsMove(int x, int y, int move_X, int move_Y)
+{
+    if((x == 0) && (y == 0))
+    {
+        return 0;
+    }
+    else if((x == move_X) && (y == move_Y))
+    {
+        return 0;
+    }
+    else if((x == 0) || (y == 0))
+    {
+        return 1;
+    }
+    else if((x == move_X - 1) || (y == move_Y - 1))
+    {
+        return 1;
+    }
+    else
+    {
+        return KingsMove(x, y - 1, move_X, move_Y) + KingsMove(x - 1, y, move_X, move_Y);
+    }
+}
+
 int main()
 {
 {
@@ -73,6 +97,23 @@ int main()
     int num = 5;
     int exp = 11;
     printf("The value of the number %d to the power of %d is %d\n", num, exp, exponentiation2(num, exp));
+}
+
+{
+    const int size_X = 4;
+    const int size_Y = 4;
+            
+    int move_X = 1;
+    int move_Y = 1;
+                    
+    for(int y = 0; y < size_Y; y++)
+    {
+        for(int x = 0; x < size_X; x++)
+        {
+            printf("%4d", KingsMove(x, y, move_X, move_Y));
+        }
+        printf("\n");
+    }
 }
 
     return 0;
